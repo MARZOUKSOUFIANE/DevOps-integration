709ab10aeca71185d9f77191248614a353e4341d
soufiane <soufianemarzouk.2017@gmail.com>
2022-06-16 00:05:12 +0100

    first commit

---
+++ src/main/java/com/devOps/DevOpsIntergationApplication.java
@@ -0,0 +1,13 @@
+package com.devOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsIntergationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsIntergationApplication.class, args);
+	}
+
+}
